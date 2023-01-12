8b64f3db429d77ede4412297d2204b4a1959450c
alex_m_s <alex_m_s@yahoo.com>
2023-01-12 10:37:47 +0100

    added hidden to title

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,27 @@ import './App.css';
 import Weather from './Weather';
 import Pokemon from './components/Pokemon'
 import LikedPokemons from "./components/LikedPokemons";
-import {Typography, Grid} from "@mui/material";
+import {Typography, Grid, Hidden} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-        <Typography variant='h1'>Pokewakey</Typography>
+        <Hidden smUp>
+            <Typography variant='h2'>Pokewakey</Typography>
+        </Hidden>
+        <Hidden smDown>
+            <Typography variant='h1'>Pokewakey</Typography>
+        </Hidden>
         <Grid container spacing={2} direction="row" justifyContent="center" alignItems="center">
           <Grid item xs={12}>
             <Weather/>
-          </Grid>   
+          </Grid>
           <Grid item xs={12} md={6}>
             <Pokemon/>
-          </Grid> 
+          </Grid>
           <Grid item xs={12} md={6}>
             <LikedPokemons/>
-          </Grid>     
+          </Grid>
         </Grid>
     </div>
   )
